9ab66fc2f87f71fba4d400480bc1fb3f080d23ac
Kristaps <azorien@gmail.com>
2022-01-13 16:17:00 +0200

    Set size for grid tiles

--- src/components/GameGrid/GameGrid.jsx
+++ src/components/GameGrid/GameGrid.jsx
@@ -4,7 +4,7 @@ import styles from './GameGrid.module.css'
 export default function GameGrid() {
   var tiles = []
   for (var i = 0; i < 100; i++) {
-    tiles.push(<div>*</div>)
+    tiles.push(<div className={styles['tile']}>*</div>)
   }
   return <div className={styles['grid']}>{tiles}</div>
 }
